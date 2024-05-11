245008814a6e98367a7942c48db970953a205f54
Lalon Sheikh <you@example.com>
2024-05-11 08:56:55 +0600

    first commit

---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,19 @@
+// import PropTypes from '.prop-types'
+import './Bottle.css'
+
+const Bottle = ({ bottle, handleAddToCart }) => {
+    const { name, img, price } = bottle;
+    return (
+        <div className="bottle">
+            <h2>Bottle : {name}</h2>
+            <img width='120rem' src={img} alt="" />
+            <p>Price : {price}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Purchase</button>
+        </div>
+    );
+};
+// Bottle.propTypes = {
+//     bottle: PropTypes.array.isRequired,
+//     handleAddToCart: PropTypes.func.isRequired
+// }
+export default Bottle;
